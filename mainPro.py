02fa59fc3617c515c484e589f6eeca11049bfcab
Suhail Sameer <54902370+suhailsameer@users.noreply.github.com>
2025-04-02 09:24:15 +0400

    Add functionality to save course data to CSV and integrate with streamLit

--- mainPro.py
+++ mainPro.py
@@ -1,16 +1,21 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from browser_use import Agent, Browser, BrowserConfig, Controller
 from pydantic import SecretStr, BaseModel
 from typing import List
 import os
 import csv
 import asyncio
+import sys
 import getpass
 from dotenv import load_dotenv
 load_dotenv()
 
-username = input("Enter your username: ")
-password = getpass.getpass("Enter your password: ")
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+#username = input("Enter your username: ")
+#password = getpass.getpass("Enter your password: ")
 api_key = os.getenv("GEMINI_API_KEY")
 
 class Course(BaseModel):
@@ -30,37 +35,6 @@ class Courses(BaseModel):
 
 controller = Controller(output_model=Courses)
 
-task = f"""
-    ### Step 1: Navigate to Website
-    Open "https://cudportal.cud.ac.ae/student/login.asp" in a web browser.
-    ### Step 2: Enter Credentials
-    Enter the following credentials:
-    - Username: {username}
-    - Password: {password}
-    ### Step 3: Login
-    Click the "Login" button.
-    ### Step 4: Access Courses
-    Click on the "Course Offerings" tab.
-    ### Step 5: Filter Courses
-    Filter the courses by selecting the "Show Filter" option.
-    Select SEAST from the division dropdown
-    Click the "Apply Filter" button.
-    ### Step 6: Obtain Courses
-    Only Obtain the following information for each course the first page (Make sure its in JSON format):
-    - Course Code
-    - Course Name
-    - Credits
-    - Instructor
-    - Room
-    - Days
-    - Start Time
-    - End Time
-    - Max Enroll
-    - Total Enroll
-    ### Step 7: Return Courses
-    Return the obtained information.
-    ```
-"""
 
 def save_to_file(data,filename="courses.csv"):
     header = ["Course Code", "Course Name", "Credits", "Instructor", "Room", "Days", "Start Time", "End Time", "Max Enroll", "Total Enroll"]
@@ -86,7 +60,41 @@ def save_to_file(data,filename="courses.csv"):
 # Initialize the model
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(os.getenv('GEMINI_API_KEY')))
 browser = Browser()
-async def main():
+
+async def main(username, password):
+    username = username
+    password = password
+    task = f"""
+    ### Step 1: Navigate to Website
+    Open "https://cudportal.cud.ac.ae/student/login.asp" in a web browser.
+    ### Step 2: Enter Credentials
+    Enter the following credentials:
+    - Username: {username}
+    - Password: {password}
+    ### Step 3: Login
+    Click the "Login" button.
+    ### Step 4: Access Courses
+    Click on the "Course Offerings" tab.
+    ### Step 5: Filter Courses
+    Filter the courses by selecting the "Show Filter" option.
+    Select SEAST from the division dropdown
+    Click the "Apply Filter" button.
+    ### Step 6: Obtain Courses
+    Only Obtain the following information for each course from pages 1-3 (Make sure its in JSON format):
+    - Course Code
+    - Course Name
+    - Credits
+    - Instructor
+    - Room
+    - Days
+    - Start Time
+    - End Time
+    - Max Enroll
+    - Total Enroll
+    ### Step 7: Return Courses
+    Return the obtained information.
+    ```
+"""
     agent = Agent(
         task=task,
         llm=llm,
@@ -98,21 +106,27 @@ async def main():
     data = results.final_result()
     if data:
         parsed: Courses = Courses.model_validate_json(data)
-        for course in parsed.courses:
-            print("\n----------------------------")
-            print(f"Course Code: {course.course_code}")
-            print(f"Course Name: {course.course_name}")
-            print(f"Credits: {course.credits}")
-            print(f"Instructor: {', '.join(course.instructor)}")
-            print(f"Room: {course.room}")
-            print(f"Days: {course.days}")
-            print(f"Start Time: {course.start_time}")
-            print(f"End Time: {course.end_time}")
-            print(f"Max Enroll: {course.max_enroll}")
-            print(f"Total Enroll: {course.total_enroll}")
-            print("----------------------------")     
-        save_to_file(parsed.courses)
-    input("Press Enter to continue...")
-    await browser.close()
+        if parsed.courses != []:
+        # for course in parsed.courses:
+        #     print("\n----------------------------")
+        #     print(f"Course Code: {course.course_code}")
+        #     print(f"Course Name: {course.course_name}")
+        #     print(f"Credits: {course.credits}")
+        #     print(f"Instructor: {', '.join(course.instructor)}")
+        #     print(f"Room: {course.room}")
+        #     print(f"Days: {course.days}")
+        #     print(f"Start Time: {course.start_time}")
+        #     print(f"End Time: {course.end_time}")
+        #     print(f"Max Enroll: {course.max_enroll}")
+        #     print(f"Total Enroll: {course.total_enroll}")
+        #     print("----------------------------")     
+            save_to_file(parsed.courses)
+            await browser.close()
+            return parsed.courses
+        else:
+            print("Not able to fetch courses")
+            await browser.close()
+    else:
+        await browser.close()
 
-asyncio.run(main())
+#asyncio.run(main(username,password))
